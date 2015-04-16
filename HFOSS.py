f0862f95b01fcbb671ba9ac1731f611860aefa42
Alex Mack <amm4108@gmail.com>
2015-04-16 12:40:19 -0400

    Added license and base code using the Sugar QuickStart generator.

---
+++ HFOSS.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import pygame
+from gi.repository import Gtk
+
+
+class HFOSS:
+    def __init__(self):
+        # Set up a clock for managing the frame rate.
+        self.clock = pygame.time.Clock()
+
+        self.x = -100
+        self.y = 100
+
+        self.vx = 10
+        self.vy = 0
+
+        self.paused = False
+        self.direction = 1
+
+    def set_paused(self, paused):
+        self.paused = paused
+
+    # Called to save the state of the game to the Journal.
+    def write_file(self, file_path):
+        pass
+
+    # Called to load the state of the game from the Journal.
+    def read_file(self, file_path):
+        pass
+
+    # The main game loop.
+    def run(self):
+        self.running = True
+
+        screen = pygame.display.get_surface()
+
+        while self.running:
+            # Pump GTK messages.
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+
+            # Pump PyGame messages.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.VIDEORESIZE:
+                    pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.direction = -1
+                    elif event.key == pygame.K_RIGHT:
+                        self.direction = 1
+
+            # Move the ball
+            if not self.paused:
+                self.x += self.vx * self.direction
+                if self.direction == 1 and self.x > screen.get_width() + 100:
+                    self.x = -100
+                elif self.direction == -1 and self.x < -100:
+                    self.x = screen.get_width() + 100
+
+                self.y += self.vy
+                if self.y > screen.get_height() - 100:
+                    self.y = screen.get_height() - 100
+                    self.vy = -self.vy
+
+                self.vy += 5
+
+            # Clear Display
+            screen.fill((255, 255, 255))  # 255 for white
+
+            # Draw the ball
+            pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 100)
+
+            # Flip Display
+            pygame.display.flip()
+
+            # Try to stay at 30 FPS
+            self.clock.tick(30)
+
+
+# This function is called when the game is run directly from the command line:
+# ./TestGame.py
+def main():
+    pygame.init()
+    pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+    game = HFOSS()
+    game.run()
+
+if __name__ == '__main__':
+    main()
