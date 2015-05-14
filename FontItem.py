8d0dac940440a165483bf97fa46c17bbe118e74a
Melody Kelly <mgk1068@rit.edu>
2015-05-13 22:50:43 -0400

    add FontItem.py

---
+++ FontItem.py
@@ -0,0 +1,37 @@
+import pygame
+
+class FontItem(pygame.font.Font):
+    def __init__(self, text, font_size=30, font=None,
+                 font_color=(0, 0, 0), pos_x=0, pos_y=0,):
+        pygame.font.Font.__init__(self, font, font_size)
+        self.text = text
+        self.font_size = font_size
+        self.font_color = font_color
+        self.label = self.render(self.text, 1, self.font_color)
+        self.width = self.label.get_rect().width
+        self.height = self.label.get_rect().height
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.position = pos_x, pos_y
+
+    def set_position(self, x, y):
+        self.position = (x, y)
+        self.pos_x = x
+        self.pos_y = y
+	self.label = self.render(self.text, 1, self.font_color)
+
+    def set_font_color(self, rgb_tuple):
+        self.font_color = rgb_tuple
+        self.label = self.render(self.text, 1, self.font_color)
+
+class FontButton(FontItem):
+    def __init__(self, text, font_size=30, font=None,
+                 font_color=(0, 0, 0), pos_x=0, pos_y=0):
+        super(FontButton, self).__init__(text, font_size, font, font_color,
+                        pos_x, pos_y)
+    def is_mouse_selection(self, posx_posy):
+        posx, posy = posx_posy
+        if (posx >= self.pos_x and posx <= self.pos_x + self.width) and \
+            (posy >= self.pos_y and posy <= self.pos_y + self.height):
+                return True
+        return False
